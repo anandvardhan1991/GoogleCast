756391158dcc72bca0922ac4e3f8bfcd7cb226a7
anandvardhan1991 <anandvardhan1991@gmail.com>
2016-07-28 14:54:37 +0530

    Second Commit

---
+++ app/src/main/java/com/naxtre/anandvardhan/gitpushexample/SecondClass.java
@@ -0,0 +1,15 @@
+package com.naxtre.anandvardhan.gitpushexample;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Anand Vardhan on 7/28/2016.
+ */
+public class SecondClass extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+
+    }
+}
